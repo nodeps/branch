71659f6f7c380d60fb9b770e9a248c0f2f307238
bruce.wang <37991842@qq.com>
2019-03-27 19:55:43 +0800

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -26,15 +26,17 @@ function branch() {
     cd nova
     git checkout $1
     cmt_nova frontend/nova-lib $1
-    cmt_nova frontend/nova-admin $1 cd ..
+    cmt_nova frontend/nova-admin $1
+    cd ..
     cd mst_state_source
     git checkout $1
-    cmt_submodule nova $1 & cd ..
+    cmt_submodule nova $1
+    cd ..
     cd mst-admin-front
     git checkout $1
     cmt_submodule nova $1
     cmt_submodule static $1
 }
-branch feature-20190328-share
-# alias branch='function cmt_nova() {echo "$1" . "$2";git pull && git submodule update && cd "$1" && git checkout "$2" && git pull && cd .. && cd .. && git commit . -m "update "$1"." && git push};function cmt_submodule() {git pull && git submodule update && cd "$1" && git checkout "$2" && git pull && cd .. && git commit . -m "update "$1"." && git push};function branch() {cd ~/Desktop/gitlab && cd nova && git checkout $1 && cmt_nova frontend/nova-lib $1 && cmt_nova frontend/nova-admin $1 cd .. && cd mst_state_source && git checkout $1 && cmt_submodule nova $1 & cd .. && cd mst-admin-front && git checkout $1 && cmt_submodule nova $1 && cmt_submodule static $1};branch'
+branch develop
+# alias branch='function cmt_nova() {echo "$1" . "$2";git pull && git submodule update && cd "$1" && git checkout "$2" && git pull && cd .. && cd .. && git commit . -m "update "$1"." && git push};function cmt_submodule() {git pull && git submodule update && cd "$1" && git checkout "$2" && git pull && cd .. && git commit . -m "update "$1"." && git push};function branch() {cd ~/Desktop/gitlab && cd nova && git checkout $1 && cmt_nova frontend/nova-lib $1 && cmt_nova frontend/nova-admin $1 && cd .. && cd mst_state_source && git checkout $1 && cmt_submodule nova $1 && cd .. && cd mst-admin-front && git checkout $1 && cmt_submodule nova $1 && cmt_submodule static $1};branch'
 
